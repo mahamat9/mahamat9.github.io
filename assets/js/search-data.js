8e13e700c5702d6de3be53d0534e833675e9d1af
mahamat9 <146455706+mahamat9@users.noreply.github.com>
2025-10-29 16:07:27 +0000

    Deploying to gh-pages from @ mahamat9/mahamat9.github.io@e3d11c4238ccfb5d0e0744986cf8ad88a4b7dea7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%6D%61%68%61%6D%61%74%6E%6F%75%72%39%39@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
